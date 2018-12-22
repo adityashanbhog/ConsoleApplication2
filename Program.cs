3fd8ad430d1b5e1c90b7b6563234b49e39c42b20
adityashanbhog <adityashanbhog32@gmail.com>
2018-12-22 21:09:27 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            Console.WriteLine(c.Sub(4,3));
+        }
+    }
+}
